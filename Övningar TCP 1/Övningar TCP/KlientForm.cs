10f70223328971bb7e251d4abbfa60bebb373c50
NiktoTM <112383619+NiktoTM@users.noreply.github.com>
2024-05-19 21:52:33 +0200

    Add files via upload

---
+++ Övningar TCP 1/Övningar TCP/KlientForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Övningar_TCP
+{
+    public partial class KlientForm : Form
+    {
+        TcpClient klient;
+        int port = 12345;
+
+        public KlientForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress adress = IPAddress.Parse(tbxIPAdress.Text);
+            klient = new TcpClient();
+            klient.NoDelay = true;
+            klient.Connect(adress, port);
+
+            if(klient.Connected)
+            {
+                byte[] utData = Encoding.Unicode.GetBytes("Hej!");
+                klient.GetStream().Write(utData, 0, utData.Length);
+                klient.Close();
+            }
+        }
+    }
+}
---
+++ Övningar TCP 1/Övningar TCP/ServerForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Övningar_TCP
+{
+    public partial class ServerForm : Form
+    {
+
+        TcpListener lyssnare;
+        TcpClient klient;
+        int port = 12345;
+
+
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lyssnare = new TcpListener(IPAdress.any, port);
+            lyssnare.Start();
+
+            klient = lyssnare.AcceptTcpClient();
+
+            byte[] inData = new byte[256];
+
+            int antalByte = klient.GetStream().Read(inData, 0, inData.Length);
+
+            tbxInkorg.Text = Encoding.Unicode.GetString(inData, 0, antalByte);
+            klient.Close();
+            lyssnare.Stop();
+        }
+    }
+}
